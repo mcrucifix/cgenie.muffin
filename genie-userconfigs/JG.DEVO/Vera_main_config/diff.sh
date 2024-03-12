86de1e19bc8907050e4563ef55941d438d84cda6
Justin Gerard (justin.gerard@uclouvain.be) <jgerard@mbackf2.cism.ucl.ac.be>
2024-03-12 15:51:22 +0100

    All the devonian user config files

---
+++ genie-userconfigs/JG.DEVO/Vera_main_config/diff.sh
@@ -0,0 +1,4 @@
+diff JG.Vera370M.main_config.SPIN JG.Vera383M.main_config.SPIN
+diff JG.Vera370M.main_config.SPIN JG.Vera393M.main_config.SPIN
+diff JG.Vera370M.main_config.SPIN JG.Vera408M.main_config.SPIN
+diff JG.Vera370M.main_config.SPIN JG.Vera420M.main_config.SPIN
